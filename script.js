8cb0accab26acbd5e53630c8d081b8f52cc2c63d
Under-The-Bridge <ramazanikbaev6@gmail.com>
2025-11-01 00:43:46 +0500

    fix some trouble

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function updateDate(){
-    let monthNames = ["январь", "февраль", "март", "апрель", "май", "июнь","июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+    let month = ["январь", "февраль", "март", "апрель", "май", "июнь","июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+    let weekDay = ["пн","вт","ср","чт","пт","сб","вс"];
     let date = new Date();
     document.querySelector("#hour").innerText = date.getHours();
     document.querySelector("#minute").innerText = date.getMinutes();
     document.querySelector("#second").innerText = date.getSeconds();
-    document.querySelector("#day").innerText = date.getDay();
-    document.querySelector("#month").innerText = `"${monthNames[date.getMonth()]}"`;
+    document.querySelector("#weekDay").innerText = `"${weekDay[date.getDay()-1]}"`;
+    document.querySelector("#day").innerText = date.getDate();
+    document.querySelector("#month").innerText = `"${month[date.getMonth()]}"`;
     document.querySelector("#year").innerText = date.getFullYear();
 }
 setInterval(updateDate,1000);
