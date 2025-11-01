477d6e9d10a3507440206eca44eb4059a5a1b4d8
Under-The-Bridge <ramazanikbaev6@gmail.com>
2025-11-01 08:52:59 +0500

    add color settings for text

--- script.js
+++ script.js
@@ -12,3 +12,46 @@ function updateDate(){
 }
 setInterval(updateDate,1000);
 updateDate();
+
+let settingsOpened = false;
+
+addEventListener("keydown",(i)=>{
+    if(i.key == "s"){
+        OpenCloseSettings();
+    }
+});
+
+function OpenCloseSettings(){
+    let settingsPanel = document.querySelector("#settingsPanel");
+    settingsOpened = !settingsOpened;
+    settingsPanel.style.transform = `translate(-35vw,${settingsOpened?0:-101}vh)`;
+}
+
+
+document.querySelector("#bgColor").addEventListener("input",()=>{
+    changeColor("#Background","#bgColor");
+});
+document.querySelector("#textColor").addEventListener("input",()=>{
+    changeColor(".Text","#textColor");
+});
+document.querySelector("#letColor").addEventListener("input",()=>{
+    changeColor(".ReservedWord","#letColor");
+});
+document.querySelector("#intColor").addEventListener("input",()=>{
+    changeColor(".Integer","#intColor");
+});
+document.querySelector("#strColor").addEventListener("input",()=>{
+    changeColor(".String","#strColor");
+});
+
+function changeColor(elem,id){
+    let element = document.querySelectorAll(elem);
+    let color = document.querySelector(id);
+    if(elem == "#Background"){
+        element.style.backgroundColor = color.value;
+    }else{
+        element.forEach(i => {
+            i.style.color = color.value;
+        });
+    }
+}
